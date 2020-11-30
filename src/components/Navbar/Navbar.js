0e8442e8d85c72b908aaf5d2bebf32e4e13cb59d
Catevika <dominique.bello@outlook.fr>
2020-11-30 18:51:49 +0100

    Initial commit after create-react-app

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import '../../css/Navbar.css';
+
+const Navbar = () => {
+	return (
+		<>
+			<h1>
+				<span className='author'>Luciano Bello</span> alias{' '}
+				<span className='author'>Catevik</span>
+			</h1>
+			<nav>
+				<a href='www.example.com'>Accueil</a>
+			</nav>
+		</>
+	);
+};
+
+export default Navbar;
